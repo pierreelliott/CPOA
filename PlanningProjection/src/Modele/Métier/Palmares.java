b64a730fca68ffbe58d2a5c1a91fc36b50600250
THIBOUD PIERRE-ELLIOTT p1402690 <p1402690@univ-lyon1.fr>
2017-01-06 16:21:51 +0100

    Avancement chargement de la BDD

--- PlanningProjection/src/Modele/Métier/Palmares.java
+++ PlanningProjection/src/Modele/Métier/Palmares.java
@@ -11,16 +11,61 @@ import java.util.*;
 
 /** @pdOid b98beedd-b294-45d8-9db8-cd0db3c91e77 */
 public class Palmares {
+    /* ======== Statique ======= */
+    private static List<Palmares> palmares;
+    
+    public static List<Palmares> getPalmares() { return palmares; }
+    public static Palmares getPalmares(String type) {
+        Iterator ite = palmares.iterator();
+        while(ite.hasNext())
+        {
+            Palmares tmp = (Palmares)ite.next();
+            if(tmp.getTypePalmares().equals(type)) return tmp;
+        }
+        return null;
+    }
+    
+    /* ========Instance ========= */
    /** @pdOid fd8f8d58-ba7b-48c6-a7f8-722a8eebceb3 */
    private String typePalmares;
+   /** @pdRoleInfo migr=no name=VIP assc=association6 mult=0..1 */
+   private VIP vIP;
+   private Film film;
+   
+   /** @pdOid f86ca549-8036-4969-8e29-54d6881d3d8d */
+   public Palmares(String type) {
+      typePalmares = type;
+      palmares.add(this);
+   }
+   public Palmares(String type, Film f) {
+      typePalmares = type;
+      film = f;
+      palmares.add(this);
+   }
+   public Palmares(String type, VIP vip) {
+      typePalmares = type;
+      vIP = vip;
+      palmares.add(this);
+   }
    
    /** @pdOid 8a86debb-b441-47a0-8fea-59fd1ddd90d0 */
    protected void finalize() {
       // TODO: implement
    }
    
-   /** @pdRoleInfo migr=no name=VIP assc=association6 mult=0..1 */
-   public VIP vIP;
+   public void setFilm(Film f) {
+       film = f;
+   }
+   public Film getFilm() {
+       return film;
+   }
+   
+   public void setVIP(VIP vip) {
+       vIP = vip;
+   }
+   public VIP getVIP() {
+       return vIP;
+   }
    
    /** @pdOid e953d2a0-2c06-4c0a-af8f-d7c8b58ff4d6 */
    public String getTypePalmares() {
@@ -33,9 +78,6 @@ public class Palmares {
       typePalmares = newTypePalmares;
    }
    
-   /** @pdOid f86ca549-8036-4969-8e29-54d6881d3d8d */
-   public Palmares() {
-      // TODO: implement
-   }
+   
 
 }
